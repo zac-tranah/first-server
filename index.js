378f7dbcb266cac89715b09fb2b93d30d0afc72b
Zac Tranah <tranahzac@gmail.comgit config --global user.name zac-tranah>
2025-12-03 10:01:59 +0000

    add .env to project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const app = require("./app")
-const port = 3001
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log(`FruityApp running on port ${port}`)
